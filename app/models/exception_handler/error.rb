ca13e12967f155efab8d095761d7117614151679
Viacheslav Molokov <viacheslav.molokov@gmail.com>
2014-10-07 17:16:18 +0400

    Support disabled ActiveRecord
    In case of not using ActiveRecord it should still work

--- app/models/exception_handler/error.rb
+++ app/models/exception_handler/error.rb
@@ -1,9 +1,16 @@
 module ExceptionHandler
-	class Error < ActiveRecord::Base
-		#Table is called "errors"
-		#Dev needs to use migration to create db
+  if defined?(ActiveRecord::Base)
+    class Error < ActiveRecord::Base
+      #Table is called "errors"
+      #Dev needs to use migration to create db
 
-		#Associations
-		belongs_to :usable, polymorphic: true
-	end
+      #Associations
+      belongs_to :usable, polymorphic: true
+    end
+  else
+    class Error
+      def self.create(*)
+      end
+    end
+  end
 end
